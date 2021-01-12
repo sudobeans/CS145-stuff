95483f5b7d6ca7b4e1f1d3ed98f2f435d3999fae
aharlan22 <77132952+aharlan22@users.noreply.github.com>
2021-01-11 20:08:02 -0800

    add giant

---
+++ Lab2/critters/Giant.java
@@ -0,0 +1,42 @@
+// This defines a class of critters that move back and forth between two walls.
+// Whenever they bump into a creature, they infect it.
+
+import java.awt.*;
+
+public class Giant extends Critter{
+
+   int steps = 0; 
+   
+   public Color getColor() {
+      return Color.GRAY;  
+   }
+ 
+   
+
+   public Action getMove(CritterInfo info) {
+      if (info.getFront().equals(Neighbor.OTHER)) {
+         steps++;
+         return Action.INFECT;
+      }else if (info.getFront() == Neighbor.SAME || 
+         info.getFront() == Neighbor.OTHER) {
+         steps++;
+         return Action.HOP;
+      } else {
+         steps++;
+         return Action.RIGHT;
+      }
+   }
+    
+   public String toString() {
+      if (steps <= 6){
+         return "fee";
+      }else if (steps >= 7 && steps <= 12){
+         return "fie";
+      }else if (steps <= 13 && steps <= 18){
+         return "foe";
+      }else if (steps <= 19 && steps >= 24){
+         return "fum";
+      } 
+   }
+    
+}
