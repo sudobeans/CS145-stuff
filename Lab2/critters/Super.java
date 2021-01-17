efd856a13fc10fc6dc950092fb312aee01606235
juddmorse <juddmorse@gmail.com>
2021-01-16 16:09:57 -0800

    removed dead chunk

--- Lab2/critters/Super.java
+++ Lab2/critters/Super.java
@@ -13,44 +13,6 @@ public class Super extends Critter {
         this.stepCounter = 0;
     }
 
-    // See the header for the orca's AI description.
-/*    public Action getMove(CritterInfo info) {
-        // Infects other creatures
-        if (info.getFront() == Neighbor.OTHER && info.getDirection() == Direction.NORTH) {
-            if (info.getFrontDirection() != Direction.SOUTH) {
-                return Action.INFECT;
-            } else return Action.LEFT;
-        } else if (info.getFront() == Neighbor.OTHER && info.getDirection() == Direction.EAST) {
-            if (info.getFrontDirection() != Direction.WEST) {
-                return Action.INFECT;
-            }
-            else return Action.LEFT;
-        } else if (info.getFront() == Neighbor.OTHER && info.getDirection() == Direction.SOUTH) {
-            if (info.getFrontDirection() != Direction.NORTH) {
-                return Action.INFECT;
-            }
-            else return Action.LEFT;
-        } else if (info.getFront() == Neighbor.OTHER && info.getDirection() == Direction.WEST) {
-            if (info.getFrontDirection() != Direction.EAST) {
-                return Action.INFECT;
-            }
-            else return Action.LEFT;
-        } else if (info.getFront() == Neighbor.EMPTY) {
-            stepCounter++;
-            return Action.HOP;
-            // turns away left
-        } else if (info.getRight() != Neighbor.EMPTY && info.getFront() != Neighbor.EMPTY) {
-            return Action.LEFT;
-            // Spins and right
-        } else if (info.getLeft() != Neighbor.EMPTY && info.getFront() != Neighbor.EMPTY) {
-            return Action.RIGHT;
-        } else {
-            stepCounter++;
-            stepCounter %= stepCycle.length; // Makes its steps loop
-            return stepCycle[stepCounter];
-        }
-    }
-*/
     public Action getMove(CritterInfo info) {
         if (info.getDirection() == Direction.NORTH) {
             if(info.getFront() == Neighbor.OTHER) {
