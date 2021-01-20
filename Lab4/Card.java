0dfa36f550c58c9a84e10fe74235a4bf46da8bf1
sudobeans <skywd7766@gmail.com>
2021-01-19 18:39:24 -0800

    add extra methods to Card

--- Lab4/Card.java
+++ Lab4/Card.java
@@ -3,12 +3,14 @@ import java.util.*;
 
 // Represents a playing card from a fifty-two card deck. Has a rank and a suit.
 public class Card {
+    // Enums for suits and ranks
     public enum Suit {
         CLUBS, DIAMONDS, HEARTS, SPADES
     }
     public enum Rank {
         ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
     }
+
     private Rank rank;
     private Suit suit;
 
@@ -27,9 +29,24 @@ public class Card {
         return this.rank;
     }
 
-    // Returns true if they have the same suit and rank
+    // Takes another card. Returns true if they have the same suit and rank
     public boolean equals(Card other) {
         return (this.getSuit() == other.getSuit() && 
                 this.getRank() == other.getRank());
+    }
+
+    // Takes a String, s, and returns the rank associated with the given string.
+    public static String rankToString(Rank r) {
+        return r.toString().toLowerCase();
+    }
+
+    // Takes a Suit, s, and returns the string associated with the suit.
+    public static String suitToString(Suit s) {
+        return s.toString().toLowerCase();
+    }
+
+    // Returns the card in the following format: "<rank> of <suit>"
+    public String toString() {
+        return rankToString(this.getRank()) + " of " + suitToString(this.getSuit());
     }
 }
