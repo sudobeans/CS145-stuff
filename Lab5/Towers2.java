44784d16f28f473640e6bc4121b07c723189610f
sudobeans <skywd7766@gmail.com>
2021-03-06 14:25:18 -0800

    Add some more comments

--- Lab5/Towers2.java
+++ Lab5/Towers2.java
@@ -11,16 +11,18 @@ public class Towers2 {
         tower(DISCS, "left", "right", "middle");
     }
 
+    // Recursively moves the discs from the StartingPoint tower to the endingPoint tower.
     public static void tower(int numberOfDiscs, String startingPoint, String endingPoint, 
                              String floaterPoint) {
 
         if (numberOfDiscs == 1) { 
-            //Base case; if there's only one disc to move,
+            // Base case; if there's only one disc to move,
             // it goes from startingPoint to endingPoint.
             System.out.println("Move disc 1 from rod " + startingPoint + " to rod " + endingPoint);
             return;
         }
-
+        // Recursive case; if there is more than one disc to move, moves the 
+        // discs between the rods.
         tower(numberOfDiscs - 1, startingPoint, floaterPoint, endingPoint);
         System.out.println("Move disc " + numberOfDiscs + " from rod " + 
                            startingPoint + " to rod " + endingPoint);
