c2b41ca45ac67dfd497adb54d8dda5b88be6338c
juddmorse <juddmorse@gmail.com>
2021-03-11 09:17:38 -0800

    built out basic QuestionNode, with constructors

--- Lab6/QuestionNode.java
+++ Lab6/QuestionNode.java
@@ -1,3 +1,20 @@
 public class QuestionNode {
-    // Add some fields and methods here!
+    
+
+    public String input; // will hold either an answer or a question
+
+    public QuestionNode yesAnswer; // left side
+    public QuestionNode noAnswer; // right side
+
+    // This constructor makes a leaf, it's used for answers
+    public QuestionNode(String input) {
+        this(input, null, null);
+    }
+
+    // This constructor makes a branch with an answer/question, a left/yes, and a right/no
+    public QuestionNode(String input, QuestionNode yesAnswer, QuestionNode noAnswer) {
+        this.input = input;
+        this.yesAnswer = yesAnswer;
+        this.noAnswer = noAnswer;
+    }
 }
