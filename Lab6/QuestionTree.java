cfe851a19cc23a4b70fde313a1f8c75eff6a6bf5
sudobeans <skywd7766@gmail.com>
2021-03-11 14:33:46 -0800

    make throwIfNull() static (this might be the greatest number of commits I have made for a single method)

--- Lab6/QuestionTree.java
+++ Lab6/QuestionTree.java
@@ -49,7 +49,7 @@ public class QuestionTree {
     }
 
     // Throws an IllegalArgumentException with the given message if the given Object is null.
-    private void throwIfNull(Object o, String message) {
+    private static void throwIfNull(Object o, String message) {
         if (o == null) {
             throw new IllegalArgumentException(message);
         }
